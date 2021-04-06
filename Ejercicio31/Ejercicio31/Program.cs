27cb3fe565cd62193c8199576c911dc0e0b469f0
diego.taub <diego.taub@accenture.com>
2021-04-06 20:59:52 -0300

    Ejercicio31

---
+++ Ejercicio31/Ejercicio31/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un intervalo de tiempo: ");
+          
+
+            if(TimeSpan.TryParse(Console.ReadLine(), out TimeSpan ts))
+            {
+                Console.Write("El ingreso no puede ser interpretado como intervalo de tiempo.");
+            }
+            else
+            {
+                Console.Write("El ingreso puede ser interpretado como intervalo de tiempo.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
